00fb2a4fdfc5b5ba06721f123e1111b883084e35
Brandon Olin <brandon@devblackops.io>
2015-11-15 23:59:52 -0800

    Add cmdlet help

--- Public/New-NSLBMonitor.ps1
+++ Public/New-NSLBMonitor.ps1
@@ -15,6 +15,106 @@ limitations under the License.
 #>
 
 function New-NSLBMonitor {
+    <#
+    .SYNOPSIS
+        Creates a new load balancer server monitor.
+
+    .DESCRIPTION
+        Creates a new load balancer server monitor.
+
+    .PARAMETER Session
+        The NetScaler session object.
+
+    .PARAMETER Name
+        The name or names of the load balancer monitors to create.
+
+    .PARAMETER Type
+        Type of monitor.
+
+    .PARAMETER Interval
+        Time interval between two successive probes.
+
+    .PARAMETER IntervalType
+        Time unit for interval type.
+
+    .PARAMETER DestinationIP
+        IP address of the service to which to send probes.
+
+    .PARAMETER DestinationPort
+        TCP or UDP port to which to send the probe.
+
+    .PARAMETER ResponseTimeout
+        Amount of time for which the appliance must wait before it marks a probe as FAILED.
+
+    .PARAMETER ResponseTimeoutType
+        Time unit for monitor response timeout.    
+
+    .PARAMETER Downtime
+        Time duration for which to wait before probing a service that has been marked as DOWN.
+
+    .PARAMETER DowntimeType
+        Time unit for the Downtime parameter. 
+
+    .PARAMETER Deviation
+        Time value added to the learned average response time in dynamic response time monitoring (DRTM). 
+
+    .PARAMETER Retries
+        Maximum number of probes to send to establish the state of a service for which a monitoring probe failed.
+
+    .PARAMETER ResponseTimeoutThreshold
+        Response time threshold, specified as a percentage of the Response Time-out parameter.
+
+    .PARAMETER AlertRetries
+        Number of consecutive probe failures after which the appliance generates an SNMP trap called monProbeFailed. 
+
+    .PARAMETER SuccessRetries
+        Number of consecutive successful probes required to transition a service's state from DOWN to UP. 
+
+    .PARAMETER FailureRetries
+        Number of retries that must fail, out of the number specified for the Retries parameter, for a service to be marked as DOWN.
+
+    .PARAMETER NetProfile
+        Name of the network profile.
+
+    .PARAMETER TOS
+        Probe the service by encoding the destination IP address in the IP TOS (6) bits.
+
+    .PARAMETER TOSID
+        The TOS ID of the specified destination IP.
+
+    .PARAMETER State
+        State of the monitor.
+
+    .PARAMETER Reverse
+        Mark a service as DOWN, instead of UP, when probe criteria are satisfied, and as UP instead of DOWN when probe criteria are not satisfied.
+
+    .PARAMETER Transparent
+        The monitor is bound to a transparent device such as a firewall or router.
+
+    .PARAMETER LRTM
+        Calculate the least response times for bound services.
+
+    .PARAMETER Secure
+        Use a secure SSL connection when monitoring a service.
+
+    .PARAMETER IPTunnel
+        Send the monitoring probe to the service through an IP tunnel.
+
+    .PARAMETER ScriptName
+        Path and name of the script to execute.
+
+    .PARAMETER DispatcherIP
+        IP address of the dispatcher to which to send the probe.
+
+    .PARAMETER DispatcherPort
+        Port number on which the dispatcher listens for the monitoring probe.
+
+    .PARAMETER ScriptArgs
+        String of arguments for the script.
+
+    .PARAMETER Passthru
+        Return the load balancer server object.
+    #>
     [cmdletbinding(SupportsShouldProcess = $true, ConfirmImpact='Low')]
     param(
         $Session = $script:nitroSession,
@@ -22,10 +122,10 @@ function New-NSLBMonitor {
         [parameter(Mandatory = $true)]
         [string[]]$Name = (Read-Host -Prompt 'Monitor name'),
 
-        [ValidateSet('PING', 'TCP', 'HTTP', 'TCP-ECV', 'HTTP-ECV', 'UDP-ECV', 'DNS', 'FTP', 'LDNS-PING', 
-            'LDNS-TCP', 'RADIUS', 'USER', 'HTTP-INLINE', 'SIP-UDP', 'LOAD', 'FTP-EXTENDED', 'SMTP', 'SNMP', 
-            'NNTP', 'MYSQL', 'MYSQL-ECV', 'MSSQL-ECV', 'ORACLE-ECV', 'LDAP', 'POP3', 'CITRIX-XML-SERVICE', 
-            'CITRIX-WEB-INTERFACE', 'DNS-TCP', 'RTSP', 'ARP', 'CITRIX-AG', 'CITRIX-AAC-LOGINPAGE', 'CITRIX-AAC-LAS', 
+        [ValidateSet('PING', 'TCP', 'HTTP', 'TCP-ECV', 'HTTP-ECV', 'UDP-ECV', 'DNS', 'FTP', 'LDNS-PING',
+            'LDNS-TCP', 'RADIUS', 'USER', 'HTTP-INLINE', 'SIP-UDP', 'LOAD', 'FTP-EXTENDED', 'SMTP', 'SNMP',
+            'NNTP', 'MYSQL', 'MYSQL-ECV', 'MSSQL-ECV', 'ORACLE-ECV', 'LDAP', 'POP3', 'CITRIX-XML-SERVICE',
+            'CITRIX-WEB-INTERFACE', 'DNS-TCP', 'RTSP', 'ARP', 'CITRIX-AG', 'CITRIX-AAC-LOGINPAGE', 'CITRIX-AAC-LAS',
             'CITRIX-XD-DDC', 'ND6', 'CITRIX-WI-EXTENDED', 'DIAMETER', 'RADIUS_ACCOUNTING', 'STOREFRONT')]
         [string]$Type = 'PING',
 
@@ -38,13 +138,13 @@ function New-NSLBMonitor {
         [ValidateScript({$_ -match [IPAddress]$_ })]
         [string]$DestinationIP,
 
+        [int]$DestinationPort,
+
         [ValidateRange(1, 20939000)]
         [int]$ResponseTimeout = 2,
 
         [ValidateSet('SEC', 'MSEC', 'MIN')]
         [string]$ResponseTimeoutType = 'SEC',
-
-        [int]$DestinationPort,
 
         [ValidateRange(1, 20939000)]
         [int]$Downtime = 30,
@@ -80,7 +180,7 @@ function New-NSLBMonitor {
         [int]$TOSID,
 
         [ValidateSet('ENABLED', 'DISABLED')]
-        [string]$state = 'ENABLED',
+        [string]$State = 'ENABLED',
 
         [ValidateSet('Yes', 'NO')]
         [string]$Reverse = 'NO',
@@ -152,7 +252,7 @@ function New-NSLBMonitor {
                 if ($PSBoundParameters.ContainsKey('TOSID')) {
                     $m.tosid = $TOSID
                 }
-                $m.state = $state
+                $m.state = $State
                 $m.reverse = $Reverse
                 $m.transparent = $Transparent
                 $m.lrtm = $LRTM
