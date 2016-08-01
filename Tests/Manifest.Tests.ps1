d71ab028035b83b2217a0aca51cea02991b17e7c
Brandon Olin <brandon@devblackops.io>
2016-07-31 23:31:53 -0700

    Update manifest tests

--- Tests/Manifest.Tests.ps1
+++ Tests/Manifest.Tests.ps1
@@ -1,43 +1,90 @@
 $moduleName = 'NetScaler'
-$modulePath = Join-Path -Path (Split-Path -Parent $MyInvocation.MyCommand.Path | Split-Path -Parent) -ChildPath $moduleName
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$root = Split-Path -Path $here -Parent
+$modulePath = Join-Path -Path $root -ChildPath $moduleName
 $manifestPath = Join-Path -Path $modulePath -Child "$moduleName.psd1"
-$manifestExists = (Test-Path -Path $manifestPath)
+$changelogPath = Join-Path -Path $root -Child "CHANGELOG.md"
 
 Describe 'Module manifest' {
     Context 'Validation' {
 
-        It 'has a manifest' {
-            $manifestExists | Should Be $true
+        $script:manifest = $null
+
+        It "has a valid manifest" {
+            {
+                $script:manifest = Test-ModuleManifest -Path $manifestPath -ErrorAction Stop -WarningAction SilentlyContinue
+            } | Should Not Throw
         }
 
-        if ($manifestExists) {
-            $manifest = Test-ModuleManifest -Path $manifestPath
-        }
-
-        It 'has a valid manifest' {
-            $manifest | Should Not Be $null
+        It "has a valid name in the manifest" {
+            $script:manifest.Name | Should Be Watchmen
         }
 
         It 'has a valid root module' {
-            $manifest.RootModule | Should Be "$moduleName.psm1"
+            $script:manifest.RootModule | Should Be "$moduleName.psm1"
         }
 
+        It "has a valid version in the manifest" {
+            $script:manifest.Version -as [Version] | Should Not BeNullOrEmpty
+        }
+    
         It 'has a valid description' {
-            $manifest.Description | Should Not BeNullOrEmpty
+            $script:manifest.Description | Should Not BeNullOrEmpty
         }
 
         It 'has a valid author' {
-            $manifest.Author | Should Not BeNullOrEmpty
+            $script:manifest.Author | Should Not BeNullOrEmpty
         }
-
+    
         It 'has a valid guid' {
             { 
-                [guid]::Parse($manifest.Guid) 
+                [guid]::Parse($script:manifest.Guid) 
             } | Should Not throw
         }
-
+    
         It 'has a valid copyright' {
-            $manifest.CopyRight | Should Not BeNullOrEmpty
+            $script:manifest.CopyRight | Should Not BeNullOrEmpty
+        }
+        
+        # Only for DSC modules
+        # It 'exports DSC resources' {
+        #     $dscResources = ($Manifest.psobject.Properties | Where Name -eq 'ExportedDscResources').Value
+        #     @($dscResources).Count | Should Not Be 0
+        # }
+
+        $script:changelogVersion = $null
+        It "has a valid version in the changelog" {
+            foreach ($line in (Get-Content $changelogPath)) {
+                if ($line -match "^\D*(?<Version>(\d+\.){1,3}\d+)") {
+                    $script:changelogVersion = $matches.Version
+                    break
+                }
+            }
+            $script:changelogVersion                | Should Not BeNullOrEmpty
+            $script:changelogVersion -as [Version]  | Should Not BeNullOrEmpty
+        }
+
+        It "changelog and manifest versions are the same" {
+            $script:changelogVersion -as [Version] | Should be ( $script:manifest.Version -as [Version] )
+        }
+
+        if (Get-Command git.exe -ErrorAction SilentlyContinue) {
+            $script:tagVersion = $null
+            It "is tagged with a valid version" -skip {
+                $thisCommit = git.exe log --decorate --oneline HEAD~1..HEAD
+
+                if ($thisCommit -match 'tag:\s*(\d+(?:\.\d+)*)') {
+                    $script:tagVersion = $matches[1]
+                }
+
+                $script:tagVersion                  | Should Not BeNullOrEmpty
+                $script:tagVersion -as [Version]    | Should Not BeNullOrEmpty
+            }
+
+            It "all versions are the same" {
+                $script:changelogVersion -as [Version] | Should be ( $script:manifest.Version -as [Version] )
+                #$script:manifest.Version -as [Version] | Should be ( $script:tagVersion -as [Version] )
+            }
         }
     }
 }
