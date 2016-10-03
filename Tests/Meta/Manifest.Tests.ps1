0b20a7d4a3620f6c9ab0ba5e752cf3294394ff05
Dominique Broeglin <dominique.broeglin@gmail.com>
2016-10-03 11:32:18 +0200

    Do not extract unreleased versions from CHANGELOG.md

--- Tests/Meta/Manifest.Tests.ps1
+++ Tests/Meta/Manifest.Tests.ps1
@@ -51,7 +51,7 @@ Describe 'Module manifest' {
         $script:changelogVersion = $null
         It "has a valid version in the changelog" {
             foreach ($line in (Get-Content $changelogPath)) {
-                if ($line -match "^\D*(?<Version>(\d+\.){1,3}\d+)") {
+                if ($line -match "^## (?<Version>(\d+\.){1,3}\d+) \(\d{4}-\d{2}-\d{2}\)") {
                     $script:changelogVersion = $matches.Version
                     break
                 }
