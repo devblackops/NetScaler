fd1f9b68584eddc048c7a3086124a796baeee772
Brandon Olin <bolin@columbia.com>
2016-10-28 16:32:21 -0700

    Install modules from PSGallery

--- psake.ps1
+++ psake.ps1
@@ -21,7 +21,7 @@ task Init {
     Get-Item ENV:BH*
 
     $modules = 'Pester', 'PSDeploy', 'PSScriptAnalyzer'
-    Install-Module $modules -Confirm:$false
+    Install-Module $modules -Repository PSGallery -Confirm:$false
     Import-Module $modules -Verbose:$false -Force
 }
 
