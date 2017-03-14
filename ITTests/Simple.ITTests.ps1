a6fdb7e223d2a11a90c357eadf18c885e5f4fd69
Dominique Broeglin <dominique.broeglin@gmail.com>
2017-03-14 22:13:24 +0100

    Added integration tests to validate URI protocol fix

--- ITTests/Simple.ITTests.ps1
+++ ITTests/Simple.ITTests.ps1
@@ -1,44 +1,35 @@
-$Nsip              = "172.16.124.11"
-$Username          = "nsroot"
-$Password          = "nsroot"
-
 Import-Module Pester
 
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
-Import-Module -Force $here\..\Netscaler
+Import-Module -Force $here\..\Netscaler\Netscaler.psd1
 . $here\TestSupport.ps1
 
+Describe "Netscaler Connection" {
+    Context "not connected" {
+        It "should disconnect implicit session" {
+            $Session = Connect-TestNetscaler
+
+            Get-NSHostname
+            Disconnect-Netscaler
+            { Get-NSHostname } | Should Throw "401 (Unauthorized)"
+        }
+
+        It "should disconnect explicit session" {
+            $Session = Connect-TestNetscaler
+
+            Get-NSHostname -Session $Session
+            Disconnect-Netscaler -Session $Session
+            { Get-NSHostname -Session $Session } | Should Throw "401 (Unauthorized)"
+        }
+    }
+
+    Context "connection" {
+    }
+}
+
 Describe "Netscaler Get-*" {
-    $Session = Connect-Netscaler -Hostname $Nsip -Credential $Credential -PassThru
+    $Session = Connect-TestNetscaler
 
-    It "should list VPN policies" {
-        $Policy = Get-NSVPNSessionPolicy
-        
-        $Policy | Should Not BeNullOrEmpty
-        $Policy.name | Should Be "SETVPNPARAMS_POL" 
-    }
-
-    It "should get VPN policy" {
-        $Policy = Get-NSVPNSessionPolicy -Name "SETVPNPARAMS_POL"
-        
-        $Policy | Should Not BeNullOrEmpty
-        $Policy.name | Should Be "SETVPNPARAMS_POL"
-    }
-
-    It "should list VPN profiles" {
-        $Policy = Get-NSVPNSessionProfile
-        
-        $Policy | Should Not BeNullOrEmpty
-        $Policy.name | Should Be "SETVPNPARAMS_ACT" 
-    }
-
-    It "should get VPN profiles" {
-        $Policy = Get-NSVPNSessionProfile -Name "SETVPNPARAMS_ACT"
-        
-        $Policy | Should Not BeNullOrEmpty
-        $Policy.name | Should Be "SETVPNPARAMS_ACT"
-    }
-    
     It "should get a certificate" {
         $Cert = Get-NSSSLCertificate -Name "ns-server-certificate"
 
@@ -50,44 +41,29 @@ Describe "Netscaler Get-*" {
         $Cert = Get-NSSSLCertificate
 
         $Cert | Should Not BeNullOrEmpty
-        $Cert.certkey | Should Be "ns-server-certificate"
-    }
-    
-    It "should list builtin responder policies" {
-        $Result = Get-NSResponderPolicy -ShowBuiltin
-        
-        $Result | Should Not BeNullOrEmpty
-        # TODO: this is fragile, it probably depends on the NS version
-        $Result.Count | Should Be 6
-    }
-    
-    It "should list builtin rewrite actions" {
-        $Result = Get-NSRewriteAction -ShowBuiltin
-        
-        $Result | Should Not BeNullOrEmpty
-        # TODO: this is fragile, it probably depends on the NS version
-        $Result.Count | Should Be 45
-    }
-
-    It "should list builtin rewrite policies" {
-        $Result = Get-NSRewritePolicy -ShowBuiltin
-        
-        $Result | Should Not BeNullOrEmpty
-        # TODO: this is fragile, it probably depends on the NS version
-        $Result.Count | Should Be 49
+        $Cert.certkey | Should Match "ns-server-certificate|ns-sftrust-certificate"
     }
 }
 
 Describe "Netscaler" {
-    $Session = Connect-Netscaler -Hostname $Nsip -Credential $Credential -PassThru
+    $Session = Connect-TestNetscaler
 
-        
     It "should add a LB server" {
         New-NSLBServer -Name 'srv-test' -IPAddress 1.2.3.4
-        
-        Compare-NSConfig $OldConf | Should Match "=> add server srv-test 1.2.3.4"       
+
+        Compare-NSConfig $OldConf | Should Match "=> add server srv-test 1.2.3.4"
     }
-    
-    BeforeEach { $OldConf = Get-NSConfig }    
-    AfterEach { Clear-NSConfig -Force }        
+
+    It "should add features" {
+        Enable-NSFeature -Force -Name "aaa", "lb", "rewrite", "ssl"
+
+        Compare-NSConfig $OldConf | Should Match "=> enable ns feature LB SSL AAA REWRITE"
+    }
+
+    BeforeEach { $OldConf = Get-NSConfig }
+    AfterEach { 
+        Clear-NSConfig -Force -Level Full 
+        # Web Logging and Surge Protection are not disabled by a config clear...
+        "wl", "sp" | Disable-NSFeature -Force
+    }
 }
--- ITTests/TestSupport.ps1
+++ ITTests/TestSupport.ps1
@@ -1,11 +1,17 @@
-$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)    
+$TestNsip              = $(if ($env:TEST_NSIP) { $env:TEST_NSIP } else { "172.16.124.10" })
+$TestUsername          = "nsroot"
+$TestPassword          = "nsroot"
 
+function Connect-TestNetscaler {
+    $SecurePassword = ConvertTo-SecureString $TestPassword -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential ($TestUsername, $SecurePassword)    
+    Connect-Netscaler -Hostname $TestNsip -Credential $Credential -PassThru   
+}
 
 function Compare-NSConfig($Old, $New = $(Get-NSConfig)) {
-    $Old = $Old | ? { Test-IsMutatingConfigLines $_ }
-    $New = $New | ? { Test-IsMutatingConfigLines $_ }
-    Compare-Object $Old $New | % {
+    $Old = $Old | Where-Object { Test-IsMutatingConfigLines $_ }
+    $New = $New | Where-Object { Test-IsMutatingConfigLines $_ }
+    Compare-Object $Old $New | ForEach-Object {
         "{0} {1}" -f $_.SideIndicator, $_.InputObject
     }
 }
