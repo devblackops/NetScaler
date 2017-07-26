d8c5a33f3108663f55a48c1cec6930eedda80361
Brandon Olin <brandon@devblackops.io>
2017-07-25 21:28:21 -0700

    Force build environment variable refresh

--- build.ps1
+++ build.ps1
@@ -20,17 +20,17 @@ function Resolve-Module {
         foreach ($moduleName in $Name) {
             $module = Get-Module -Name $moduleName -ListAvailable -Verbose:$false
             Write-Verbose -Message "Resolving [$($ModuleName)]"
-            
+
             if ($module) {
                 if ($PSBoundParameters.ContainsKey('UpdateModules')) {
                     $version = $module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-                    $galleryVersion = Find-Module -Name $moduleName -Repository PSGallery -Verbose:$false | 
-                        Measure-Object -Property Version -Maximum | 
+                    $galleryVersion = Find-Module -Name $moduleName -Repository PSGallery -Verbose:$false |
+                        Measure-Object -Property Version -Maximum |
                         Select-Object -ExpandProperty Maximum
 
-                    if ($version -lt $galleryVersion) {                                        
+                    if ($version -lt $galleryVersion) {
                         Write-Verbose -Message "$($moduleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($galleryVersion.tostring())]"
-                        
+
                         Install-Module -Name $moduleName -Repository PSGallery -Verbose:$false -Force
                         Import-Module -Name $moduleName -Verbose:$false -Force -RequiredVersion $galleryVersion
                     }
@@ -53,11 +53,11 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 'BuildHelpers', 'psake', 'Pester', 'PSDeploy', 'PSScriptAnalyzer' | Resolve-Module -Update:$UpdateModules
 
 if ($PSBoundParameters.ContainsKey('Help')) {
-    Get-PSakeScriptTasks -buildFile "$PSScriptRoot\psake.ps1"   
+    Get-PSakeScriptTasks -buildFile "$PSScriptRoot\psake.ps1"
     return
 }
 
-Set-BuildEnvironment
+Set-BuildEnvironment -Force
 
 Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -nologo -Verbose:$VerbosePreference
 exit ( [int]( -not $psake.build_success ) )
