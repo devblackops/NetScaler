a05d50b413d041ba73d904d8d7ce034110f19929
Iain <iainbrighton@hotmail.com>
2019-01-22 09:54:15 +0000

    Fixes incorrect Remove-NSRDPClientProfile function name

--- NetScaler/Public/Remove-NSRDPClientProfile.ps1
+++ NetScaler/Public/Remove-NSRDPClientProfile.ps1
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 #>
 
-function Remove-NSRRDPClientProfile {
+function Remove-NSRDPClientProfile {
     <#
     .SYNOPSIS
         Removes an existing RDP client profile.
